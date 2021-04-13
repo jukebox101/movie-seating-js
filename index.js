216ff83b8e3689781e9f97fbbfe9adcd3f77607f
jukebox101 <leonora.squires@gmail.com>
2021-04-12 23:54:50 -0400

    displays selected seats and price

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const container = document.querySelector('.container');
-const availableSeats = document.querySelectorAll('.row .seats:not (.occupied)');
+const availableSeats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-let ticketPrice = +movieSelect.value;
+let ticketPrice = parseInt(movieSelect.value);
+
+function increaseSeatCount () {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    count.innerText = selectedSeats.length;
+    total.innerText = ticketPrice * selectedSeats.length;
+}
+
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat') && !(e.target.classList.contains('occupied'))) {
+        e.target.classList.toggle('selected');
+        increaseSeatCount();
+    }
+
+});
+
+
+
