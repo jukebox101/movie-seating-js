ebf8b57db3fc256aa7aea2ca3436868d3f8df5b2
jukebox101 <leonora.squires@gmail.com>
2021-04-14 09:40:45 -0400

    set seating data to localStorage

--- index.js
+++ index.js
@@ -6,24 +6,43 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = parseInt(movieSelect.value);
 
-function increaseTotalCount () {
+function populateUI (){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    console.log(selectedSeats);
+}
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+function increaseSeatTotalCount () {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-    const seatLength = selectedSeats.length;
+    const seatArr = [...selectedSeats].map(seat => [...availableSeats].indexOf(seat));
 
+    localStorage.setItem('selectedSeats', JSON.stringify(seatArr));
+
+    const seatLength = selectedSeats.length;
+    
     count.innerText = seatLength;
     total.innerText = ticketPrice * seatLength;
 }
 
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = parseInt(e.target.value);
-    increaseTotalCount();
+    
+    setMovieData(e.target.selectedIndex, e.target.value);
+
+    increaseSeatTotalCount();
 });
 
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !(e.target.classList.contains('occupied'))) {
         e.target.classList.toggle('selected');
-        increaseTotalCount();
+        increaseSeatTotalCount();
     }
 });
 
